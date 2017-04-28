42d014efd366ade1651b06bc4309ad217400a3e7
Ricardo Arguello <ricardo.arguello@soportelibre.com>
2017-04-28 09:14:42 -0500

    Se apunta a 'servicio.firmadigital.gob.ec'

--- src/main/java/ec/gob/firmadigital/api/ClienteRestServicioDocumento.java
+++ src/main/java/ec/gob/firmadigital/api/ClienteRestServicioDocumento.java
@@ -32,7 +32,7 @@ import javax.ws.rs.core.MediaType;
 public class ClienteRestServicioDocumento {
 
     // FIXME: Externalize
-    private static String REST_SERVICE_URL = "http://localhost:8080/servicio/documentos";
+    private static String REST_SERVICE_URL = "https://servicio.firmadigital.gob.ec/servicio/documentos";
 
     public String obtenerDocumento(String token) {
         Client client = ClientBuilder.newClient();
