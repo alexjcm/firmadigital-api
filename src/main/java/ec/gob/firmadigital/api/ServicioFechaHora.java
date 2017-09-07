7a27e76ec7441aad20c05e4dda749e81fcd21a69
Ricardo Arguello <ricardo.arguello@soportelibre.com>
2017-09-07 09:22:43 -0500

    Implementación inicial de servicio para obterner la fecha y hora del servidor

---
+++ src/main/java/ec/gob/firmadigital/api/ServicioFechaHora.java
@@ -0,0 +1,53 @@
+/*
+ * Firma Digital: API
+ * Copyright 2017 Secretaría Nacional de la Administración Pública
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ec.gob.firmadigital.api;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Este servicio permite obtener la fecha y hora del servidor en formato
+ * ISO-8601.
+ * 
+ * @author Ricardo Arguello <ricardo.arguello@soportelibre.com>
+ */
+@Path("/fecha-hora")
+public class ServicioFechaHora {
+
+    /**
+     * Retorna la fecha y hora del servidor, en formato ISO-8601. Por ejemplo:
+     * "2017-08-27T17:54:43.562-05:00"
+     * 
+     * @return
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getFechaHora() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("{ \"fecha_hora\" : \"");
+        sb.append(ZonedDateTime.now().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME));
+        sb.append("\" }\n");
+        return sb.toString();
+    }
+}
