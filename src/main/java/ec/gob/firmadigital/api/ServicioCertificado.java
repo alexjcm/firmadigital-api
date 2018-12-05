252176636f0930ff94e882b0c9b3cf5eba133631
Misael Fernández <misael.fernandez@mintel.gob.ec>
2018-12-05 17:06:56 -0500

    -Aplicar formato y mantenimiento en variables

--- src/main/java/ec/gob/firmadigital/api/ServicioCertificado.java
+++ src/main/java/ec/gob/firmadigital/api/ServicioCertificado.java
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package ec.gob.firmadigital.api;
 
 import java.math.BigInteger;
@@ -39,30 +38,32 @@ import javax.ws.rs.core.MediaType;
 @Path("/certificado")
 public class ServicioCertificado {
 
-	// Servicio REST interno
-	private static final String REST_SERVICE_URL = "https://ws.firmadigital.gob.ec/servicio/certificado";
+    // Servicio REST interno
+    private static final String REST_SERVICE_URL = "https://ws.firmadigital.gob.ec/servicio/certificado";
 
-	@GET
-	@Path("/revocado/{serial}")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String validarCertificado(@PathParam("serial") BigInteger serial) {
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target(REST_SERVICE_URL + "/revocado").path("{serial}").resolveTemplate("serial",
-				serial);
-		Builder builder = target.request(MediaType.TEXT_PLAIN);
-		Invocation invocation = builder.buildGet();
-		return invocation.invoke(String.class);
-	}
+    @GET
+    @Path("/revocado/{serial}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String validarCertificado(@PathParam("serial") BigInteger serial) {
+//        Client client = TrustManagerUtil.getCliente();
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(REST_SERVICE_URL + "/revocado").path("{serial}").resolveTemplate("serial",
+                serial);
+        Builder builder = target.request(MediaType.TEXT_PLAIN);
+        Invocation invocation = builder.buildGet();
+        return invocation.invoke(String.class);
+    }
 
-	@GET
-	@Path("/fechaRevocado/{serial}")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String validarFechaRevocado(@PathParam("serial") BigInteger serial) {
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target(REST_SERVICE_URL + "/fechaRevocado").path("{serial}").resolveTemplate("serial",
-				serial);
-		Builder builder = target.request(MediaType.TEXT_PLAIN);
-		Invocation invocation = builder.buildGet();
-		return invocation.invoke(String.class);
-	}
+    @GET
+    @Path("/fechaRevocado/{serial}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String validarFechaRevocado(@PathParam("serial") BigInteger serial) {
+//        Client client = TrustManagerUtil.getCliente();
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(REST_SERVICE_URL + "/fechaRevocado").path("{serial}").resolveTemplate("serial",
+                serial);
+        Builder builder = target.request(MediaType.TEXT_PLAIN);
+        Invocation invocation = builder.buildGet();
+        return invocation.invoke(String.class);
+    }
 }
--- src/main/java/ec/gob/firmadigital/api/ServicioFirmaDigital.java
+++ src/main/java/ec/gob/firmadigital/api/ServicioFirmaDigital.java
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package ec.gob.firmadigital.api;
 
 import javax.ws.rs.BadRequestException;
@@ -50,65 +49,74 @@ import javax.ws.rs.core.Response.Status;
 @Path("/firmadigital")
 public class ServicioFirmaDigital {
 
-	// Servicio REST interno
-	private static final String REST_SERVICE_URL = "https://ws.firmadigital.gob.ec/servicio/documentos";
+    // Servicio REST interno
+    private static final String REST_SERVICE_URL = "https://ws.firmadigital.gob.ec/servicio/documentos";
 
-	/**
-	 * Obterner un documento mediante una invocación REST a ws.firmadigital.gob.ec
-	 *
-	 * @param token
-	 * @return
-	 */
-	@GET
-	@Path("{token}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response obtenerDocumentos(@PathParam("token") String token) {
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target(REST_SERVICE_URL).path("{token}").resolveTemplate("token", token);
-		Builder builder = target.request(MediaType.APPLICATION_JSON);
-		Invocation invocation = builder.buildGet();
+    /**
+     * Obterner un documento mediante una invocación REST a
+     * ws.firmadigital.gob.ec
+     *
+     * @param token
+     * @return
+     */
+    @GET
+    @Path("{token}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response obtenerDocumentos(@PathParam("token") String token) {
+//        Client client = TrustManagerUtil.getCliente();
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(REST_SERVICE_URL).path("{token}").resolveTemplate("token", token);
+        Builder builder = target.request(MediaType.APPLICATION_JSON);
+        Invocation invocation = builder.buildGet();
 
-		try {
-			String json = invocation.invoke(String.class);
-			return Response.ok(json).header("Content-Length", json.length()).build();
-		} catch (BadRequestException e) {
-			String mensaje = e.getResponse().readEntity(String.class);
-			return Response.status(Status.BAD_REQUEST).type(MediaType.TEXT_PLAIN).entity(mensaje).build();
-		} catch (WebApplicationException e) {
-			String mensaje = e.getResponse().readEntity(String.class);
-			return Response.status(Status.INTERNAL_SERVER_ERROR).type(MediaType.TEXT_PLAIN).entity(
-					"Error al invocar servicio de obtencion de documentos en ws.firmadigital.gob.ec: " + mensaje)
-					.build();
-		}
-	}
+        try {
+            String json = invocation.invoke(String.class);
+            return Response.ok(json).header("Content-Length", json.length()).build();
+        } catch (BadRequestException e) {
+            String mensaje = e.getResponse().readEntity(String.class);
+            return Response.status(Status.BAD_REQUEST).type(MediaType.TEXT_PLAIN).entity(mensaje).build();
+        } catch (WebApplicationException e) {
+            String mensaje = e.getResponse().readEntity(String.class);
+            return Response.status(Status.INTERNAL_SERVER_ERROR).type(MediaType.TEXT_PLAIN).entity(
+                    "Error al invocar servicio de obtencion de documentos en ws.firmadigital.gob.ec: " + mensaje)
+                    .build();
+        }
+    }
 
-	/**
-	 * Actualizar un documento mediante una invocación REST a ws.firmadigital.gob.ec
-	 *
-	 * @param token
-	 * @param json
-	 * @return
-	 */
-	@PUT
-	@Path("{token}")
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response actualizarDocumentos(@PathParam("token") String token, String json) {
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target(REST_SERVICE_URL).path("{token}").resolveTemplate("token", token);
-		Builder builder = target.request();
-		Invocation invocation = builder.buildPut(Entity.json(json));
+    /**
+     * Actualizar un documento mediante una invocación REST a
+     * ws.firmadigital.gob.ec
+     *
+     * @param token
+     * @param json
+     * @return
+     */
+    @PUT
+    @Path("{token}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response actualizarDocumentos(@PathParam("token") String token, String json) {
+//        Client client = TrustManagerUtil.getCliente();
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(REST_SERVICE_URL).path("{token}").resolveTemplate("token", token);
+        Builder builder = target.request();
+        Invocation invocation = builder.buildPut(Entity.json(json));
 
-		try {
-			String jsonResponse = invocation.invoke(String.class);
-			return Response.ok(jsonResponse).header("Content-Length", jsonResponse.length()).build();
-		} catch (BadRequestException e) {
-			String mensaje = e.getResponse().readEntity(String.class);
-			return Response.status(Status.BAD_REQUEST).type(MediaType.TEXT_PLAIN).entity(mensaje).build();
-		} catch (WebApplicationException e) {
-			String mensaje = e.getResponse().readEntity(String.class);
-			return Response.status(Status.INTERNAL_SERVER_ERROR).type(MediaType.TEXT_PLAIN).entity(
-					"Error al invocar servicio de obtencion de documentos en ws.firmadigital.gob.ec: " + mensaje)
-					.build();
-		}
-	}
+        try {
+            String jsonResponse = invocation.invoke(String.class
+            );
+            return Response.ok(jsonResponse).header("Content-Length", jsonResponse.length()).build();
+
+        } catch (BadRequestException e) {
+            String mensaje = e.getResponse().readEntity(String.class
+            );
+            return Response.status(Status.BAD_REQUEST).type(MediaType.TEXT_PLAIN).entity(mensaje).build();
+
+        } catch (WebApplicationException e) {
+            String mensaje = e.getResponse().readEntity(String.class
+            );
+            return Response.status(Status.INTERNAL_SERVER_ERROR).type(MediaType.TEXT_PLAIN).entity(
+                    "Error al invocar servicio de obtencion de documentos en ws.firmadigital.gob.ec: " + mensaje)
+                    .build();
+        }
+    }
 }
